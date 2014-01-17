c999db7c4047245308048eb5057b9ffe356a54d0
juliegeng97 <haomiaogeng@stmarksschool.org>
2014-01-17 09:20:13 -0500

    Working parser!!

--- Stax xml parser/src/parser.java
+++ Stax xml parser/src/parser.java
@@ -10,10 +10,12 @@ import javax.xml.stream.XMLStreamReader;
 
 public class parser {
 	public static void main(String[] args) throws XMLStreamException, Exception {
-		List<Bond> bondList = null;
-		List<CDXML_Node> nodeList = null;
+		List<Bond> bondList = new ArrayList<Bond>();
+		List<CDXML_Node> nodeList = new ArrayList<CDXML_Node>();
 		CDXML_Node currNode = null;
 		Bond currBond = null;
+		String tagContent = null; 
+		
 		XMLInputFactory factory = XMLInputFactory.newInstance();
 		FileInputStream fis = new FileInputStream("xml/Terephthalic acid.cdxml");
 		XMLStreamReader reader = factory.createXMLStreamReader(fis); 
@@ -26,31 +28,32 @@ public class parser {
 				if ("n".equals(reader.getLocalName())) {
 					currNode = new CDXML_Node();
 					currNode.id = reader.getAttributeValue(null, "id");
-				}
-
-				if ("n".equals(reader.getLocalName())) {
-					nodeList = new ArrayList<CDXML_Node>();
+					currNode.Element=reader.getAttributeValue(null, "Element"); 
 				}
 
 				if ("b".equals(reader.getLocalName())) {
 					currBond = new Bond();
 					currBond.id = reader.getAttributeValue(null, "id");
-				}
-
-				if ("b".equals(reader.getLocalName())) {
-					bondList = new ArrayList<Bond>();
+					currBond.Order=reader.getAttributeValue(null, "Order");
 				}
 				break;
 
 			case XMLStreamConstants.END_ELEMENT:
-				if ("n".equals(reader.getLocalName())) {
-					nodeList.add(currNode);
-				}
-				
-				if ("b".equals(reader.getLocalName())) {
-					bondList.add(currBond);
-				}
+			switch(reader.getLocalName()){
+			case "n":
+				nodeList.add(currNode);
 				break;
+			case "b":
+				bondList.add(currBond);
+				break;
+			case "Order":
+				currBond.Order = tagContent;
+				break;
+			case "Element":
+				currNode.Element = tagContent;
+				break; 
+			}
+			break; 
 				
 			case XMLStreamConstants.START_DOCUMENT:
 				bondList = new ArrayList<Bond>();
@@ -63,16 +66,31 @@ public class parser {
 		for (Bond bond : bondList){
 			System.out.println(bond);
 		}
+		
+		for(CDXML_Node node: nodeList){
+			System.out.println(node);
+		}
+	
 	}
 
 	static class Bond {
 		String id;
 		String Order;
+		
+		public String toString(){
+			return id + " "+ Order; 
+		}
 	}
 
 	static class CDXML_Node {
 		String id;
 		String p;
 		String Z;
+		String Element; 
+		
+		public String toString(){
+			return id + " "+ Element; 
+		}
 	}
+
 }
