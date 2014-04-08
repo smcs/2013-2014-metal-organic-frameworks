8a9f65e63ef76b78260f73d41099f40ac05d1aa1
Seth Battis <SethBattis@stmarksschool.org>
2014-04-08 11:28:24 -0400

    Extracting Playtime from Work
    Moved Alex's experiments with Jung into its own project.
    
    Signed-off-by: Seth Battis <SethBattis@stmarksschool.org>

---
+++ Alex is Messing Around with Jung/src/objloader/JOGLApp.java
@@ -0,0 +1,29 @@
+package objloader;
+
+import javax.swing.JComponent;
+import javax.vecmath.Point3f;
+import javax.xml.stream.XMLStreamException;
+
+import simplerjogl.Frame;
+
+
+
+public class JOGLApp
+{
+	 
+	public static void main (String[] args) throws XMLStreamException, Exception
+	{
+		
+		parser p = new parser();
+		JOGLRenderer renderer = new JOGLRenderer ();
+		//StructureOrganizer S = new StructureOrganizer(p);
+		//GraphDemo g = new GraphDemo(); 
+		//JUNGExample j = new JUNGExample(); 
+		
+		Frame frame = Frame.createFrame ("SimplerJOGL App", false);
+		frame.addGLEventListener (renderer);
+		frame.start ();
+		
+		
+	}
+}
